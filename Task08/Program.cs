898e780a26b68af97042453e36f8900e094292ed
Michkasov Roman <mihjob@list.ru>
2022-10-16 14:54:57 +0300

    Added a text of code

--- Task08/Program.cs
+++ Task08/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+Console.Clear();
+Console.Write("Введите число a = ");
+int number = int.Parse(Console.ReadLine()!);
+
+int n = 2;
+
+Console.Write($"Четные числа до {number}: ");
+while (n <= number)
+{
+    Console.Write($"{n} ");
+    n = n + 2;
+}
