49945ebfd2db78229f4502cd6c3225d091bafb04
Michkasov Roman <mihjob@list.ru>
2022-10-18 21:37:52 +0300

    Changed a text of a code

--- Task04/Program.cs
+++ Task04/Program.cs
@@ -8,16 +8,9 @@ int number2 = int.Parse(Console.ReadLine()!);
 Console.Write("Введите третье число с = ");
 int number3 = int.Parse(Console.ReadLine()!);
 
-if ((number1 > number2) & (number2 > number3))
+if ((number1 > number2) && (number1 > number3))
     Console.Write ($"Из введенных вами чисел наибольшим является число {number1}");
-if ((number1 > number3) & (number3 > number2))
-    Console.Write ($"Из введенных вами чисел наибольшим является число {number1}");
-if ((number2 > number1) & (number1 > number3))
+else if (number2 > number3)
     Console.Write ($"Из введенных вами чисел наибольшим является число {number2}");
-if ((number2 > number3) & (number3 > number1))
-    Console.Write ($"Из введенных вами чисел наибольшим является число {number2}");
-if ((number3 > number1) & (number1 > number2))
+else 
     Console.Write ($"Из введенных вами чисел наибольшим является число {number3}");
-if ((number3 > number2) & (number2 > number1))
-    Console.Write ($"Из введенных вами чисел наибольшим является число {number3}");   
-    
